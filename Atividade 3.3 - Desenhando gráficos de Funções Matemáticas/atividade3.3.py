900a4d7be7542fef371bd09eb6fc4113ddf70589
Renan Martins <renanrodm@gmail.com>
2026-09-14 12:05:58 -0300

    Adiciona função quadrática

--- Atividade 3.3 - Desenhando gráficos de Funções Matemáticas/atividade3.3.py
+++ Atividade 3.3 - Desenhando gráficos de Funções Matemáticas/atividade3.3.py
@@ -14,12 +14,14 @@
 
 # Fazer uma função para rodar uma "corrida de tartarugas" para N tartarugas (a função só receberá o N como parâmetro).
 from turtle import *
+from time import sleep
 
 
 t = Turtle()
 
 def desenha_plano_cartesiano(x, y):
     #Eixo x
+    t.color("black")
     t.pu()
     t.setheading(0)
     t.goto(-x, 0)
@@ -34,9 +36,22 @@ def desenha_plano_cartesiano(x, y):
     t.pd()
     t.goto(0, y)
     t.stamp()
+    t.setheading(0)
+
+def eleva_ao_quadrado(x):
+    t.color("red")
+    return x**2
 
 
+t.speed(0)
 
-desenha_plano_cartesiano(300, 300)
+desenha_plano_cartesiano(250, 250)
+
+t.pu()
+t.goto(-20*10, eleva_ao_quadrado(-20))
+t.pd()
+for x in range(-20, 21):
+    t.goto(10*x, eleva_ao_quadrado(x))
+
 
 mainloop()
