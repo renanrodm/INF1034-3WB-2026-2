b48a3ff178b6508e4ccc18a94468758086d662ea
Renan Martins <renanrodm@gmail.com>
2026-09-14 11:44:11 -0300

    Adiciona funcao para plano cartesiano

--- Atividade 3.3 - Desenhando gráficos de Funções Matemáticas/atividade3.3.py
+++ Atividade 3.3 - Desenhando gráficos de Funções Matemáticas/atividade3.3.py
@@ -1,3 +1,42 @@
+# PRINCIPAL (400XP)
+
+# Criar uma função para desenhar o plano cartesiano (25XP);
+# Devem ser desenhadas as seguintes funções matemáticas:
+#     - y = √x (50XP)
+#     - y = 1/x (50XP)
+#     - y = 2^x (50XP)
+#     - y = 5 - x^2 (75XP)
+#     - y = x^2 - 5x + 6 (75XP)
+#     - y = x^3 - x^2 - x + 1 (75XP)
+# OBS.: Dar o clear depois de desenhar cada função.
+
+# EXTRA (100XP)
+
+# Fazer uma função para rodar uma "corrida de tartarugas" para N tartarugas (a função só receberá o N como parâmetro).
+from turtle import *
+
+
+t = Turtle()
+
+def desenha_plano_cartesiano(x, y):
+    #Eixo x
+    t.pu()
+    t.setheading(0)
+    t.goto(-x, 0)
+    t.pd()
+    t.goto(x, 0)
+    t.stamp()
+
+    #Eixo y
+    t.pu()
+    t.left(90)
+    t.goto(0, -y)
+    t.pd()
+    t.goto(0, y)
+    t.stamp()
 
 
 
+desenha_plano_cartesiano(300, 300)
+
+mainloop()
